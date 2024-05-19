cab682653d277984bce3cea0383018b46feaf8a0
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2024-05-19 23:11:15 +0600

    feat: Add userSession utility module
    This commit adds a new utility module called userSession.ts. It exports a Map object named userSessions, which will be used to store user session data. This module will be utilized by other parts of the application to manage user sessions.

---
+++ src/utils/userSeassion.ts
@@ -0,0 +1,7 @@
+import { UserSession } from "../app/modules/Scrapper/scrapper.interface";
+
+//User sessions
+const userSessions: Map<string, UserSession> = new Map();
+
+
+export default userSessions;
